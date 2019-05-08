ebe379c209ff6629c032603c7e082792289829b9
S. Michael Hussain <smhussain_1@yahoo.com>
2019-05-08 14:56:44 -0700

    work prior to window load code almost done

--- Backupindex.js
+++ Backupindex.js
@@ -3,9 +3,8 @@
 const youtube_key = "AIzaSyDPmO5VRwaw1PS1caF2ZXpg1cXaEtjtWSM";
 const youtube_baseurl = "";
 
-// just creates the variable of the search term
+// creates the variable of the search term
 function userSearch() {
-    console.log('test');
     $('.head').on('click', function(event) {  
         event.preventDefault();    
         let entry = $(this).text();
@@ -53,30 +52,32 @@ function showYouTube(results) {
         
         html += '<table><tr><td><a href="https://www.youtube.com/watch?v=' + value.id.videoId + '?vq=hd1080" data-lity>' + 
         '<div class="thumbnailWrap"><img class="thumbnail" src="' + value.snippet.thumbnails.medium.url + 
-        '"><!--<p class="play"><i class="fa fa-play-circle" aria-hidden="true"></i><br><span class="popup">Play here</span></p>--></div></a></td>' + 
+        '"></div></a></td>' + 
         '<td class="tdtext"><a href="https://www.youtube.com/watch?v=' + value.id.videoId + '?vq=hd1080" data-lity><p class="videotitle">' + 
         title + '</p></a><p class="videochannel">' + 
-        value.snippet.publishedAt.substring(0, value.snippet.publishedAt.length - 14).replace(/-/g, '/') + ' &middot; <a href="https://www.youtube.com/channel/' + value.snippet.channelId + '" target="_blank">' +
-        value.snippet.channelTitle + '</a></p><!--<a class="read_description" href="#v' + index + '" rel="modal:open"><i class="fa fa-question-circle" aria-hidden="true"></i> Read description</a>--><p class="fulldescription" id="v' + index + '" style="display: none;"><span class="fd_title"><i class="fa fa-question-circle" aria-hidden="true"></i> ' + value.snippet.title + '</span><span class="fd_by">Posted by <a href="https://www.youtube.com/channel/' + value.snippet.channelId + '" target="_blank">' +
+        value.snippet.publishedAt.substring(0, value.snippet.publishedAt.length - 14).replace(/-/g, '/') + ' &middot; <a href="https://www.youtube.com/channel/' + value.snippet.channelId + 'target="_blank">' +
+        value.snippet.channelTitle + '</a></p></h1><p class="fulldescription" id="v' + index + '" style="display: none;"><span class="fd_title"><i class="fa fa-question-circle" aria-hidden="true"></i> ' + value.snippet.title + '</span><span class="fd_by">Posted by <a href="https://www.youtube.com/channel/' + value.snippet.channelId + 'target="_blank">' +
         value.snippet.channelTitle + '</a> on '+ value.snippet.publishedAt.substring(0, value.snippet.publishedAt.length - 14).replace(/-/g, '/') +'</span>' + value.snippet.description + '<a class="fd_link" href="https://www.youtube.com/watch?v=' + value.id.videoId + '?vq=hd1080" data-lity rel="modal:close"> Watch the video to learn more<i>!</i></a></p><p class="videodescription">' + value.snippet.description.substring(0, 110).trim() + '...' + '</p></td></tr></table>';
       });
-      //console.log(html);
-      $('.youtube').html(html); // changed the append here to html, and below
+      $('.youtube').html(html); 
       $('.external-link').html('<p class="ext_link"><a href="https://www.youtube.com/results?search_query=' + $('.head').text() + 
-        '" target="_blank"><i class="fa fa-external-link-square" aria-hidden="true"></i> &nbsp;More on YouTube</a></p>');
+        'target="_blank"><i class="fa fa-external-link-square" aria-hidden="true"></i> &nbsp;More on YouTube</a></p>');
       $('.results-page').removeClass('hidden');
       $('.search-again').html('<a href="#page-top">Back to top of page</a>');
     }
 }
 
+// Plugs the Race Name Selected by User into the Header above the YouTube Results
 function showHeader(entry) {
   $('.main-header').html('<h1>'+ entry + '</h1>');
 }
+
 
 function autoScroll() {
   let heightToScroll = $('.topContainer').outerHeight();
       $('html').animate({scrollTop: heightToScroll});
 }
+
 
 $(function() {
     console.log('App loaded! Waiting for submit!');
